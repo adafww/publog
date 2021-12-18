de9b89d61eedcb04c417e0427c81970528d4df4e
mxhx23@gmail.com <s4S8cNxZR!NcAch>
2021-12-18 12:07:49 +0300

    adding jpql queries fix

--- src/main/java/main/repository/PostRepository.java
+++ src/main/java/main/repository/PostRepository.java
@@ -10,39 +10,47 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     @Query("select " +
-            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, sum(case v.value when true then 1 else 0 end), sum(case v.value when false then 1 else 0 end), count(c.postId.id), p.viewCount) " +
+            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, " +
+            "(select count(v) from v where v.value = true and v.post.id = p.id), " +
+            "(select count(v) from v where v.value = false and v.post.id = p.id), " +
+            "(select count(c) from c where c.postId.id = p.id), p.viewCount) " +
             "from Post p " +
-            "inner join User u on p.user.id = u.id " +
-            "inner join PostVote v on p.id = v.post.id " +
+            "inner join PostVote v on p = v.post " +
             "inner join PostComment c on p.id = c.postId.id " +
             "where p.moderationStatusType='ACCEPTED' " +
-            "group by p.id order by p.time desc")
+            "group by p order by p.time desc")
     List<PostForDtoRepository> findRecent(PageRequest of);
     @Query("select " +
-            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, sum(case v.value when true then 1 else 0 end), sum(case v.value when false then 1 else 0 end), count(c.postId.id), p.viewCount) " +
+            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, " +
+            "(select count(v) from v where v.value = true and v.post.id = p.id), " +
+            "(select count(v) from v where v.value = false and v.post.id = p.id), " +
+            "(select count(c) from c where c.postId.id = p.id), p.viewCount) " +
             "from Post p " +
-            "inner join User u on p.user.id = u.id " +
             "inner join PostVote v on p.id = v.post.id " +
             "inner join PostComment c on p.id = c.postId.id " +
             "where p.moderationStatusType='ACCEPTED' " +
             "group by p.id order by count (c.postId) desc")
     List<PostForDtoRepository> findPopular(PageRequest of);
     @Query("select " +
-            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, sum(case v.value when true then 1 else 0 end), sum(case v.value when false then 1 else 0 end), count(c.postId.id), p.viewCount) " +
+            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, " +
+            "(select count(v) from v where v.value = true and v.post.id = p.id), " +
+            "(select count(v) from v where v.value = false and v.post.id = p.id), " +
+            "(select count(c) from c where c.postId.id = p.id), p.viewCount) " +
             "from Post p " +
-            "inner join User u on p.user.id = u.id " +
             "inner join PostVote v on p.id = v.post.id " +
             "inner join PostComment c on p.id = c.postId.id " +
             "where p.moderationStatusType='ACCEPTED' and v.value = true " +
             "group by p.id order by count (v.post) desc")
     List<PostForDtoRepository> findBest(PageRequest of);
     @Query("select " +
-            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, sum(case v.value when true then 1 else 0 end), sum(case v.value when false then 1 else 0 end), count(c.postId.id), p.viewCount) " +
+            "new main.dto.PostForDtoRepository(p.id, p.time, p.user.id, p.title, p.text, " +
+            "(select count(v) from v where v.value = true and v.post.id = p.id), " +
+            "(select count(v) from v where v.value = false and v.post.id = p.id), " +
+            "(select count(c) from c where c.postId.id = p.id), p.viewCount) " +
             "from Post p " +
-            "inner join User u on p.user.id = u.id " +
-            "inner join PostVote v on p.id = v.post.id " +
+            "inner join PostVote v on p = v.post " +
             "inner join PostComment c on p.id = c.postId.id " +
             "where p.moderationStatusType='ACCEPTED' " +
-            "group by p.id order by p.time asc")
+            "group by p order by p.time asc")
     List<PostForDtoRepository> findEarly(PageRequest of);
 }
