5c5b79ec9eda4660d31664e06e3a7d1b985212c8
mxhx23@gmail.com <s4S8cNxZR!NcAch>
2022-02-01 08:53:21 +0300

    fix

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
